42fb127a677aca2647abab893fae4cb45dc87a17
jaroszw <jaroszw@gmail.com>
2023-12-06 15:16:04 +0100

    custom middlewares

--- server/server.js
+++ server/server.js
@@ -1,20 +1,25 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
+const { logger } = require("./middleware/logger");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 dotenv.config();
+app.use(logger);
+app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
-const app = express();
+console.log(path.join(__dirname, "./views", "404.html"));
 
-app.use("/", express.static(path.join(__dirname, "/public")));
+app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/", require("./routes/root"));
 
 app.all("*", (req, res) => {
   res.status(404);
   console.log(req.accepts("html"));
   if (req.accepts("html")) {
-    res.sendFile(path.join(__dirname, "./views", "404.html"));
+    res.sendFile(path.join(__dirname, "views", "404.html"));
   } else if (req.accepts("json")) {
     res.json({ message: "404 Not found" });
   } else {
