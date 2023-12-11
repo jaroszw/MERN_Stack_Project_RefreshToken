07ee822db70871d00352563ae87557414c80b753
jaroszw <jaroszw@gmail.com>
2023-12-11 13:08:58 +0100

    user controllers added

--- server/server.js
+++ server/server.js
@@ -19,8 +19,10 @@ app.use(logger);
 app.use(cookieParser());
 app.use(express.json());
 
+// ROUTING
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/", require("./routes/root"));
+app.use("/users", require("./routes/user"));
 
 app.all("*", (req, res) => {
   res.status(404);
